f8242eacb2db7b4edba6e65d3a18f4f599eb6b8b
HellBoys <irwin.noteam.2009@gmail.com>
2019-01-14 01:02:42 +0300

    add println for debug

--- reader_test.go
+++ reader_test.go
@@ -1,1 +1,41 @@
 package bitstream
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+)
+
+func TestBitReader(t *testing.T) {
+	data := []byte{0x65} // 01100101
+
+	testCases := []struct {
+		readBit     bool
+		count       int
+		expectedArr []byte
+		expected    uint64
+	}{
+		{
+			readBit: true,
+			count:   6,
+		},
+	}
+
+	for _, test := range testCases {
+		r := NewReader(bytes.NewBuffer(data))
+		fmt.Printf("data: %b\n", data)
+		var b byte
+		var err error
+		if test.readBit {
+			b, _ := r.ReadBits(test.count)
+			fmt.Printf("b: %b\n", b)
+		} else {
+			b, err = r.ReadByte()
+		}
+		if err != nil {
+			t.Fatal(err)
+		}
+		fmt.Printf("b: %b\n", b)
+	}
+	t.Fatal("OK")
+}
